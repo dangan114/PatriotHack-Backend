5ea1c7d26a79b537c186487f710b551dce9aab62
AnDang2003 <an.dang114@gmail.com>
2023-03-26 01:02:06 -0400

    message response test

--- index.js
+++ index.js
@@ -205,6 +205,14 @@ app.post('/sms', async (req, res) => {
       }).status(200))
 })
 
+app.post('/response', (req, res) => {
+    const twiml = new MessagingResponse();
+
+    twiml.message('The Robots are coming! Head for the hills!');
+
+    res.type('text/xml').send(twiml.toString());
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
